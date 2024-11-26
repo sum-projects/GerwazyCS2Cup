ac3f56ce05875e47799a79ebc827940d5e734a3c
sUm <jakub.sumara09@gmail.com>
2024-11-26 09:29:52 +0100

    New matches

--- js/statystki.js
+++ js/statystki.js
@@ -133,7 +133,7 @@ const json = `{
         {"name": "ncl.", "kills": 40, "deaths": 19, "assists": 4, "psg": 52, "damage": 3697, "win": false},
         {"name": "[-_-?]", "kills": 25, "deaths": 22, "assists": 6, "psg": 44, "damage": 2517, "win": false},
         {"name": "Pugiano ZDR", "kills": 20, "deaths": 24, "assists": 11, "psg": 40, "damage": 2284, "win": false},
-        {"name": "KaLoSz CASE.GIFT", "kills": 17, "deaths": 24, "assists": 7, "psg": 29, "damage": 1845, "win": false},
+        {"name": "KINIORSZCZYK", "kills": 17, "deaths": 24, "assists": 7, "psg": 29, "damage": 1845, "win": false},
         {"name": "GarbatyProPlayer", "kills": 12, "deaths": 23, "assists": 9, "psg": 25, "damage": 1737, "win": false}
       ],
       "screenshot": "mecz1.png"
@@ -151,7 +151,7 @@ const json = `{
         {"name": "ncl.", "kills": 16, "deaths": 13, "assists": 4, "psg": 68, "damage": 1707, "win": false},
         {"name": "Dario", "kills": 13, "deaths": 17, "assists": 2, "psg": 30, "damage": 1425, "win": false},
         {"name": "GarbatyProPlayer", "kills": 10, "deaths": 14, "assists": 5, "psg": 30, "damage": 1188, "win": false},
-        {"name": "KaLoSz CASE.GIFT", "kills": 11, "deaths": 16, "assists": 2, "psg": 27, "damage": 1080, "win": false}
+        {"name": "KINIORSZCZYK", "kills": 11, "deaths": 16, "assists": 2, "psg": 27, "damage": 1080, "win": false}
       ],
       "screenshot": "mecz2.png"
     },
@@ -165,7 +165,7 @@ const json = `{
         {"name": "Mixon2t", "kills": 12, "deaths": 9, "assists": 3, "psg": 58, "damage": 1208, "win": true},
         {"name": "Kundi", "kills": 9, "deaths": 11, "assists": 6, "psg": 33, "damage": 1015, "win": true},
         {"name": "ncl.", "kills": 17, "deaths": 13, "assists": 3, "psg": 23, "damage": 1987, "win": false},
-        {"name": "KaLoSz CASE.GIFT", "kills": 10, "deaths": 14, "assists": 1, "psg": 30, "damage": 1072, "win": false},
+        {"name": "KINIORSZCZYK", "kills": 10, "deaths": 14, "assists": 1, "psg": 30, "damage": 1072, "win": false},
         {"name": "Dario", "kills": 7, "deaths": 13, "assists": 4, "psg": 71, "damage": 1053, "win": false},
         {"name": "[-_-?]", "kills": 7, "deaths": 14, "assists": 2, "psg": 42, "damage": 658, "win": false},
         {"name": "GarbatyProPlayer", "kills": 2, "deaths": 14, "assists": 2, "psg": 0, "damage": 554, "win": false}
@@ -183,7 +183,7 @@ const json = `{
         {"name": "SebaPrzeCh", "kills": 7, "deaths": 13, "assists": 7, "psg": 14, "damage": 877, "win": true},
         {"name": "Stefanek", "kills": 18, "deaths": 15, "assists": 2, "psg": 33, "damage": 1973, "win": false},
         {"name": "ncl.", "kills": 15, "deaths": 15, "assists": 1, "psg": 66, "damage": 1537, "win": false},
-        {"name": "KaLoSz CASE.GIFT", "kills": 6, "deaths": 17, "assists": 6, "psg": 83, "damage": 1128, "win": false},
+        {"name": "KINIORSZCZYK", "kills": 6, "deaths": 17, "assists": 6, "psg": 83, "damage": 1128, "win": false},
         {"name": "AfterWait", "kills": 6, "deaths": 17, "assists": 2, "psg": 66, "damage": 836, "win": false},
         {"name": "GarbatyProPlayer", "kills": 6, "deaths": 17, "assists": 2, "psg": 33, "damage": 720, "win": false}
       ],
@@ -199,7 +199,7 @@ const json = `{
         {"name": "Stefanek", "kills": 15, "deaths": 14, "assists": 8, "psg": 50, "damage": 1804, "win": true},
         {"name": "GarbatyProPlayer", "kills": 13, "deaths": 16, "assists": 4, "psg": 38, "damage": 1167, "win": true},
         {"name": "ncl.", "kills": 25, "deaths": 17, "assists": 5, "psg": 40, "damage": 2547, "win": false},
-        {"name": "KaLoSz CASE.GIFT", "kills": 15, "deaths": 21, "assists": 7, "psg": 33, "damage": 1902, "win": false},
+        {"name": "KINIORSZCZYK", "kills": 15, "deaths": 21, "assists": 7, "psg": 33, "damage": 1902, "win": false},
         {"name": "BlaQu", "kills": 14, "deaths": 20, "assists": 5, "psg": 71, "damage": 1706, "win": false},
         {"name": "Kundi", "kills": 14, "deaths": 21, "assists": 10, "psg": 57, "damage": 1578, "win": false},
         {"name": "pereK", "kills": 13, "deaths": 19, "assists": 1, "psg": 7, "damage": 1175, "win": false}
@@ -211,7 +211,7 @@ const json = `{
       "date": "2024-11-25",
       "players": [
         {"name": "ncl.", "kills": 27, "deaths": 6, "assists": 4, "psg": 62, "damage": 2556, "win": true},
-        {"name": "KaLoSz CASE.GIFT", "kills": 20, "deaths": 9, "assists": 5, "psg": 35, "damage": 2237, "win": true},
+        {"name": "KINIORSZCZYK", "kills": 20, "deaths": 9, "assists": 5, "psg": 35, "damage": 2237, "win": true},
         {"name": "Stefanek", "kills": 12, "deaths": 13, "assists": 7, "psg": 50, "damage": 1431, "win": true},
         {"name": "GarbatyProPlayer", "kills": 5, "deaths": 14, "assists": 8, "psg": 40, "damage": 951, "win": true},
         {"name": "pereK", "kills": 13, "deaths": 11, "assists": 0, "psg": 61, "damage": 880, "win": true},
@@ -222,6 +222,74 @@ const json = `{
         {"name": "Kundi", "kills": 7, "deaths": 15, "assists": 0, "psg": 14, "damage": 707, "win": false}
       ],
       "screenshot": "mecz6.png"
+    },
+        {
+      "match_id": 7,
+      "date": "2024-11-22",
+      "players": [
+        {"name": "HESOYAM", "kills": 26, "deaths": 16, "assists": 7, "psg": 38, "damage": 2824, "win": true},
+        {"name": "ncl.", "kills": 19, "deaths": 20, "assists": 7, "psg": 73, "damage": 2270, "win": true},
+        {"name": "[-[__?-]", "kills": 18, "deaths": 16, "assists": 3, "psg": 72, "damage": 1739, "win": true},
+        {"name": "GarbatyProPlayer", "kills": 13, "deaths": 17, "assists": 7, "psg": 23, "damage": 1516, "win": true},
+        {"name": "Pugiano ZDR", "kills": 12, "deaths": 16, "assists": 4, "psg": 58, "damage": 1146, "win": true},
+        {"name": "Koniu", "kills": 26, "deaths": 16, "assists": 10, "psg": 38, "damage": 2755, "win": false},
+        {"name": "BlaQu", "kills": 22, "deaths": 16, "assists": 4, "psg": 45, "damage": 2244, "win": false},
+        {"name": "Golden Sum", "kills": 18, "deaths": 11, "assists": 3, "psg": 33, "damage": 2069, "win": false},
+        {"name": "KINIORSZCZYK", "kills": 8, "deaths": 17, "assists": 7, "psg": 37, "damage": 1099, "win": false},
+        {"name": "Mixon2t", "kills": 11, "deaths": 19, "assists": 2, "psg": 45, "damage": 988, "win": false}
+      ],
+      "screenshot": "mecz7.png"
+    },
+    {
+      "match_id": 8,
+      "date": "2024-11-22",
+      "players": [
+        {"name": "ncl.", "kills": 31, "deaths": 17, "assists": 8, "psg": 61, "damage": 3156, "win": true},
+        {"name": "HESOYAM", "kills": 22, "deaths": 16, "assists": 4, "psg": 27, "damage": 2195, "win": true},
+        {"name": "Pugiano ZDR", "kills": 13, "deaths": 17, "assists": 9, "psg": 69, "damage": 1802, "win": true},
+        {"name": "Golden Sum", "kills": 20, "deaths": 14, "assists": 3, "psg": 25, "damage": 1761, "win": true},
+        {"name": "GarbatyProPlayer", "kills": 7, "deaths": 16, "assists": 4, "psg": 14, "damage": 1024, "win": true},
+        {"name": "[-[__?-]", "kills": 18, "deaths": 20, "assists": 5, "psg": 61, "damage": 2049, "win": false},
+        {"name": "pereK", "kills": 15, "deaths": 18, "assists": 5, "psg": 46, "damage": 1806, "win": false},
+        {"name": "Dario", "kills": 20, "deaths": 16, "assists": 2, "psg": 50, "damage": 1701, "win": false},
+        {"name": "KINIORSZCZYK", "kills": 13, "deaths": 21, "assists": 5, "psg": 23, "damage": 1634, "win": false},
+        {"name": "Mixon2t", "kills": 14, "deaths": 19, "assists": 6, "psg": 50, "damage": 1605, "win": false}
+      ],
+      "screenshot": "mecz8.png"
+    },
+    {
+      "match_id": 9,
+      "date": "2024-11-22",
+      "players": [
+        {"name": "KINIORSZCZYK", "kills": 19, "deaths": 13, "assists": 4, "psg": 26, "damage": 1902, "win": true},
+        {"name": "[-[__?-]", "kills": 19, "deaths": 12, "assists": 3, "psg": 57, "damage": 1889, "win": true},
+        {"name": "Dario", "kills": 18, "deaths": 13, "assists": 3, "psg": 33, "damage": 1796, "win": true},
+        {"name": "Pugiano ZDR", "kills": 14, "deaths": 11, "assists": 9, "psg": 28, "damage": 1721, "win": true},
+        {"name": "ncl.", "kills": 10, "deaths": 14, "assists": 3, "psg": 70, "damage": 1188, "win": true},
+        {"name": "HESOYAM", "kills": 11, "deaths": 17, "assists": 2, "psg": 18, "damage": 1597, "win": false},
+        {"name": "Mixon2t", "kills": 13, "deaths": 17, "assists": 4, "psg": 30, "damage": 1575, "win": false},
+        {"name": "Golden Sum", "kills": 12, "deaths": 16, "assists": 7, "psg": 33, "damage": 1474, "win": false},
+        {"name": "GarbatyProPlayer", "kills": 12, "deaths": 15, "assists": 3, "psg": 16, "damage": 1145, "win": false},
+        {"name": "pereK", "kills": 12, "deaths": 15, "assists": 2, "psg": 50, "damage": 1072, "win": false}
+      ],
+      "screenshot": "mecz9.png"
+    },
+    {
+      "match_id": 10,
+      "date": "2024-11-22",
+      "players": [
+        {"name": "Golden Sum", "kills": 27, "deaths": 12, "assists": 2, "psg": 29, "damage": 2416, "win": true},
+        {"name": "HESOYAM", "kills": 25, "deaths": 13, "assists": 3, "psg": 44, "damage": 2151, "win": true},
+        {"name": "[-[__?-]", "kills": 16, "deaths": 15, "assists": 6, "psg": 62, "damage": 1882, "win": true},
+        {"name": "BlaQu", "kills": 6, "deaths": 15, "assists": 10, "psg": 100, "damage": 1203, "win": true},
+        {"name": "Mixon2t", "kills": 8, "deaths": 17, "assists": 8, "psg": 50, "damage": 1150, "win": true},
+        {"name": "KINIORSZCZYK", "kills": 17, "deaths": 17, "assists": 10, "psg": 23, "damage": 2220, "win": false},
+        {"name": "ncl.", "kills": 17, "deaths": 14, "assists": 2, "psg": 47, "damage": 1579, "win": false},
+        {"name": "Pugiano ZDR", "kills": 16, "deaths": 18, "assists": 6, "psg": 56, "damage": 1554, "win": false},
+        {"name": "Koniu", "kills": 15, "deaths": 16, "assists": 4, "psg": 26, "damage": 1502, "win": false},
+        {"name": "GarbatyProPlayer", "kills": 6, "deaths": 17, "assists": 9, "psg": 16, "damage": 897, "win": false}
+      ],
+      "screenshot": "mecz10.png"
     }
   ]
 }`;
